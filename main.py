6848417e0dcf3ba425ba87d4d5ebc26bf23443a2
Nick D. Pattengale <ndpatte@gmail.com>
2024-07-28 17:39:13 -0600

    update main.py

--- main.py
+++ main.py
@@ -50,9 +50,7 @@ async def merge_hyfiles(the_arg):
                 cnt += 1
                 f.write(line)
         hf = hytek_parser.parse_hy3("a.hy3")
-        output_div.innerText = dir(hf)
-        output_div.innerText += " :: %d" % cnt
-        output_div.innerText += " -- " + str(hf.__attrs_attrs__)
+        output_div.innerText = hf.meet
         for event_key in hf.meet.events.keys():
             event_record = hf.meet.events[event_key]
             if event_key not in d:
