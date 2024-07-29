f6f464237472e82c703e711003bd4f64cd966624
Nick D. Pattengale <ndpatte@gmail.com>
2024-07-29 11:47:20 -0600

    calc some file info

--- drag_drop.js
+++ drag_drop.js
@@ -39,64 +39,21 @@ function handleDrop(e) {
   handleFiles(files)
 }
 
-let uploadProgress = []
-let progressBar = document.getElementById('progress-bar')
-
-function initializeProgress(numFiles) {
-  progressBar.value = 0
-  uploadProgress = []
-
-  for(let i = numFiles; i > 0; i--) {
-    uploadProgress.push(0)
-  }
-}
-
-function updateProgress(fileNumber, percent) {
-  uploadProgress[fileNumber] = percent
-  let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-  progressBar.value = total
-}
-
 function handleFiles(files) {
     files = [...files]
-  initializeProgress(files.length)
-  files.forEach(uploadFile)
   files.forEach(previewFile)
 }
 
 function previewFile(file) {
   let reader = new FileReader()
-  reader.readAsDataURL(file)
+  reader.readAsBinaryString(file)
   reader.onloadend = function() {
-    let img = document.createElement('img')
-    img.src = reader.result
-    document.getElementById('gallery').appendChild(img)
+    let p = document.createElement('p')
+      //img.src = reader.result
+      p.innerText = file
+      p.innerText += " "
+      p.innerText += CryptoJS.MD5(reader.result)
+    document.getElementById('gallery').appendChild(p)
   }
-}
-
-function uploadFile(file, i) {
-  var url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload'
-  var xhr = new XMLHttpRequest()
-  var formData = new FormData()
-  xhr.open('POST', url, true)
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-
-  // Update progress (can be used to show progress indicator)
-  xhr.upload.addEventListener("progress", function(e) {
-    updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
-  })
-
-  xhr.addEventListener('readystatechange', function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      updateProgress(i, 100) // <- Add this
-    }
-    else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Error. Inform the user
-    }
-  })
-
-  formData.append('upload_preset', 'ujpu6gyk')
-  formData.append('file', file)
-  xhr.send(formData)
 }
 
